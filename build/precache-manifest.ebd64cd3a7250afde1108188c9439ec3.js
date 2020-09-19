95bd5d4f2e841c855dc0aeac9c55a211a42aed4c
Michele Cynowicz <michele.cynowicz@voxmedia.com>
2020-09-19 09:52:46 -0400

    deploy

---
+++ build/precache-manifest.ebd64cd3a7250afde1108188c9439ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eae4b282754f1a5ee2cb96bec523f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f99b13fc90dd2145488a",
+    "url": "/static/css/main.8afbd951.chunk.css"
+  },
+  {
+    "revision": "765874714a3d7737d2e4",
+    "url": "/static/js/2.6b30f7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b30f7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99b13fc90dd2145488a",
+    "url": "/static/js/main.0e170711.chunk.js"
+  },
+  {
+    "revision": "c9f21619192f511ed008",
+    "url": "/static/js/runtime-main.a9773bf2.js"
+  }
+]);
